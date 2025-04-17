4216e98b3ce0fc21d7f3578243ff23870b8ef68a
tracer-nju <tracer@nju.edu.cn>
2025-04-17 23:42:50 +0800

    Linux dydong-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:50 up  2:32,  1 user,  load average: 0.55, 0.50, 0.45

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -1,11 +1,78 @@
 #include "co.h"
 #include <stdlib.h>
+#include <stdint.h> 
+#define STACK_SIZE 1024*64
+#define MAX_CO 1<<10
 
-struct co {
+#ifdef LOCAL_MACHINE
+    #define debug(...) printf(__VA_ARGS__)
+#else
+    #define debug(...)
+#endif
+
+struct co *current = NULL;
+struct co *co_list[MAX_CO];
+int co_count = 0;
+
+enum co_status {
+    CO_NEW = 1, // create a co
+    CO_RUNNING, // running
+    CO_WAITING, // on wait
+    CO_DEAD,    // has finished
 };
 
+struct context {
+    uint64_t rsp;
+    uint64_t rip;
+    uint64_t rbx;
+    uint64_t rbp;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+};
+
+struct co {
+    char *name; // co's name
+    void (*func)(void *); // co's entry place
+    void *arg;  // co's arg
+
+    enum co_status status;  // co's state
+    struct co *    waiter;  // other waiters
+    struct context context; // save co's regs
+    uint8_t stack[STACK_SIZE];  // co's stack point
+};
+
+void co_trampoline()
+{
+    current->status = CO_RUNNING;
+    current->func(current->arg);
+    current->status = CO_DEAD;
+    co_yield();
+}
+
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-    return NULL;
+    // init the new_co
+    struct co *new_co = malloc(sizeof(struct co));
+    memset(new_co,0,sizeof(struct co));
+    new_co->name = strdup(name);
+    new_co->func = func;
+    new_co->arg = arg;
+    new_co->status = CO_NEW;
+    
+    // calculate the stack point
+    uintptr_t sp = (uintptr_t)(new_co->stack+STACK_SIZE);
+    sp = sp-(sp % 16);
+    printf("This co's stack point is: %x to %x\n",new_co->stack,sp);
+
+    // push the co_trampoline's frame
+    sp -= sizeof(void*);
+    *(void**)sp = (void*)co_trampoline;
+    new_co->context.rsp = sp;
+    new_co->context.rip = (uint64_t)co_trampoline;
+    co_list[co_count++] = new_co;
+
+    return new_co;
 }
 
 void co_wait(struct co *co) {
