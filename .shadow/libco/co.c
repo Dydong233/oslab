2bc86bd521fd7151cf7beecd071d788085f7faa7
tracer-nju <tracer@nju.edu.cn>
2025-04-21 15:13:40 +0800

    Linux dydong-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:40 up  4:53,  1 user,  load average: 0.55, 0.41, 0.47

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -67,26 +67,21 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     return new_co;
 }
 
-void co_wait(struct co *co)
-{
-	current->status = CO_WAITING;
-	co->waiter = current;
-	while (co->status != CO_DEAD)
-	{
-		co_yield ();
-	}
-	current->status = CO_RUNNING;
-	struct co *h = current;
-
-	while (h)
-	{
-		if (h->next == co)
-			break;
-		h = h->next;
-	}
-	//从环形链表中删除co
-	h->next = h->next->next;
-	free(co);
+void co_wait(struct co *co) {
+    current->status = CO_WAITING;
+    co->waiter = current;
+    while(co->status != CO_DEAD)    co_yield();
+    current->status = CO_RUNNING;
+    // delete co from the list
+    struct co *h = current;
+    while(h){
+        if(h->next == co){
+            h->next = co->next;
+            break;
+        }
+        h = h->next;
+    }
+    free(co);
 }
 
 void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
@@ -105,24 +100,42 @@ void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
     );
 }
 
-void co_yield() {
-    int val = setjmp(current->context);
-    // choose anther co and switch to it
-    if(!val){
-        // choose new or running co
-        struct co *co_next = current;
-        do{
-            co_next = co_next->next;
-        }
-        while(co_next->status == CO_DEAD || co_next->status == CO_WAITING);
-        current = co_next;
-        if(current->status == CO_NEW){
-            assert(current->status == CO_NEW);
-            ((struct co volatile *)current)->status = CO_RUNNING;
-            stack_switch_call(&current->stack[STACK_SIZE], current->func,(uintptr_t)current->arg);
-            ((struct co volatile *)current)->status = CO_DEAD;
-            if(current->waiter) current = current->waiter;
-        }
-        else longjmp(current->context, 1);
-    }
+void co_yield ()
+{
+	if (current == NULL) // init main
+	{
+		current = (struct co *)malloc(sizeof(struct co));
+		current->status = CO_RUNNING;
+		strcpy(current->name, "main");
+		current->next = current;
+	}
+	assert(current);
+	int val = setjmp(current->context);
+	if (val == 0) // co_yield() 被调用时，setjmp 保存寄存器现场后会立即返回 0，此时我们需要选择下一个待运行的协程 (相当于修改 current)，并切换到这个协程运行。
+	{
+		// choose co_next
+		struct co *co_next = current;
+		do
+		{
+			co_next = co_next->next;
+		} while (co_next->status == CO_DEAD || co_next->status == CO_WAITING);
+		current = co_next;
+		if (co_next->status == CO_NEW)
+		{
+			assert(co_next->status == CO_NEW);
+			((struct co volatile *)current)->status = CO_RUNNING; //  fogot!!!
+			stack_switch_call(&current->stack[STACK_SIZE], current->func, (uintptr_t)current->arg);
+			((struct co volatile *)current)->status = CO_DEAD;
+			if (current->waiter)
+				current = current->waiter;
+		}
+		else
+		{
+			longjmp(current->context, 1);
+		}
+	}
+	else // longjmp returned(1) ,don't need to do anything
+	{
+		return;
+	}
 }
