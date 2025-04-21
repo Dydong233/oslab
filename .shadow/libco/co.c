b48557a23a79d19779c32f922f63974c47e1dd8c
tracer-nju <tracer@nju.edu.cn>
2025-04-21 14:16:45 +0800

    Linux dydong-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:16:45 up  3:56,  1 user,  load average: 0.70, 0.60, 0.41

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -39,16 +39,6 @@ struct co {
     uint8_t stack[STACK_SIZE];  // co's stack point
 };
 
-void co_trampoline()
-{
-    if(current->status == CO_DEAD) return;
-    current->status = CO_RUNNING;
-    current->func(current->arg);
-    current->status = CO_DEAD;
-    alive_co--;
-    co_yield();
-}
-
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     struct co *new_co = (struct co *)malloc(sizeof(struct co));
     memset(new_co, 0, sizeof(struct co));
@@ -79,125 +69,9 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 }
 
 void co_wait(struct co *co) {
-    // situation 1: co is dead
-    if(co->status == CO_DEAD) {
-        printf("co %s is dead\n", co->name);
-        free(co);
-        return;
-    }
-    // situation 2: co is running
-    while(co->status != CO_DEAD) {
-        co_yield();
-    }
-    free(co);
-}
 
-__attribute__((noinline))
-int setjmp(struct context *ctx)
-{
-#if __x86_64__
-    asm volatile(
-        // save the rsp&rip&other regs
-        "movq %%rsp, 0(%0)\n\t"
-        "leaq 1f(%%rip), %%rax\n\t"
-        "movq %%rax, 8(%0)\n\t"
-        "movq %%rbx, 16(%0)\n\t"
-        "movq %%rbp, 24(%0)\n\t"
-        "movq %%r12, 32(%0)\n\t"
-        "movq %%r13, 40(%0)\n\t"
-        "movq %%r14, 48(%0)\n\t"
-        "movq %%r15, 56(%0)\n\t"
-        "xor %%eax, %%eax\n\t"
-        "1:\n\t"
-        : /* no output */
-        : "r"(ctx)
-        : "memory", "rax"
-    );
-#else
-    asm volatile(
-        "movl %%esp, 0(%0)\n\t"
-        "call 1f\n\t"
-        "1:\n\t"
-        "popl %%eax\n\t"
-        "movl %%eax, 4(%0)\n\t"
-        "movl %%ebx, 8(%0)\n\t"
-        "movl %%ebp, 12(%0)\n\t"
-        "movl %%esi, 16(%0)\n\t"
-        "movl %%edi, 20(%0)\n\t"
-        "xorl %%eax, %%eax\n\t"
-        :
-        : "r"(ctx)
-        : "memory", "eax"
-    );
-#endif
-    return 0;
-}
-__attribute__((noinline))
-void longjmp(struct context *ctx)
-{
-#if __x86_64__
-    asm volatile(
-         "movq 16(%0), %%rbx\n\t"
-         "movq 24(%0), %%rbp\n\t"
-         "movq 32(%0), %%r12\n\t"
-         "movq 40(%0), %%r13\n\t"
-         "movq 48(%0), %%r14\n\t"
-         "movq 56(%0), %%r15\n\t"
-         "movq 0(%0), %%rsp\n\t"
-         "jmp *8(%0)\n\t"
-         :
-         : "r"(ctx)
-         : "memory"
-    );
-#else
-    asm volatile(
-        "movl 8(%0), %%ebx\n\t"
-        "movl 12(%0), %%ebp\n\t"
-        "movl 16(%0), %%esi\n\t"
-        "movl 20(%0), %%edi\n\t"
-        "movl 0(%0), %%esp\n\t"
-        "jmp *4(%0)\n\t"
-        :
-        : "r"(ctx)
-        : "memory"
-    );
-#endif
-    __builtin_unreachable(); // never return
 }
 
 void co_yield() {
-    if(!alive_co)   return;
-
-    // save current co's context(This co must be running!)
-    if(current != NULL){
-        if(setjmp(&current->context) == 0) {
-            if(current->status != CO_DEAD)    current->status = CO_WAITING;
-        }
-    }
-
-    // find the next co
-    struct co *next = NULL;
-    int rand_co;
-    while(233) {
-        // The first situation : current is NULL
-        // The second situation : There is only one co
-        rand_co = rand() % co_count;
-        if(co_list[rand_co]->status != CO_DEAD) {
-            next = co_list[rand_co];
-            // printf("This time choose co %s\n", next->name);
-            break;
-        }
-    }
-    // if the next co is new
-    if(next->status == CO_NEW){
-        next->status = CO_RUNNING;
-        
-    }
-
-
-    // switch to next co
-    if(next!=NULL)  current = next;
-    current->status = CO_RUNNING;
-    longjmp(&current->context);
-    return;
+    
 }
