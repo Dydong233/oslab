b895285ec0fdb1b85af045c566157503642672ff
tracer-nju <tracer@nju.edu.cn>
2025-04-23 11:13:07 +0800

    Linux dydong-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:13:07 up  1:11,  1 user,  load average: 1.21, 0.74, 0.58

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -121,7 +121,8 @@ void co_yield() {
     if(!val){
         // choose new or running co
         struct co *co_next = current;
-        int rand_num = rand()%10;
+        int rand_num = rand();
+        rand_num = rand_num % 10;
         // int rand_num = 10;
 
         do{
