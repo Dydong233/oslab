331cb2f7ed4d0a72af0e05493ee35a3f8a2d0ab1
tracer-nju <tracer@nju.edu.cn>
2025-05-04 14:51:03 +0800

    Linux dydong-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:03 up  4:10,  1 user,  load average: 1.40, 1.26, 1.05

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,16 +1,25 @@
 #include <common.h>
 
 #define PAGE_SIZE 4096
+#define Slab_num 1024
 #define DEBUG 1
 
 static int isinit = 0;
 uintptr_t start_addr, end_addr, size;
 
+// Use slab_page to make detailed divisions
+typedef struct slab_page{
+    bool is_used[128];  // waste
+    size_t obj_count;
+    size_t obj_size;
+    size_t used_count;
+}slab_page;
+// Use slab_info to save different sizes of slabs
 struct Slab_Info{
     uintptr_t start;
     uintptr_t end;
     size_t size;
-
+    slab_page page[Slab_num];
 }slab_info[5];
 
 int two_pow(int n)
@@ -22,20 +31,30 @@ int two_pow(int n)
 }
 void init_memory()
 {
+    // init heap
     start_addr = (uintptr_t)heap.start;
     end_addr = (uintptr_t)heap.end;
     size = end_addr - start_addr;
+    // init slab: O(5*Slab_num*128)
     for(int i = 0; i < 5; i++)
     {
         slab_info[i].size = two_pow(i+5);
-        slab_info[i].start = start_addr + i * PAGE_SIZE * 1024;  // every slab is 4MB
-        slab_info[i].end = slab_info[i].start + PAGE_SIZE * 1024; // every slab is 4MB
-
+        slab_info[i].start = start_addr + i * PAGE_SIZE * Slab_num;  // every slab is 4MB
+        slab_info[i].end = slab_info[i].start + PAGE_SIZE * Slab_num;
+        for(int j=0; j < Slab_num; j++)
+        {
+            slab_info[i].page[j].obj_size = slab_info[i].size;
+            slab_info[i].page[j].obj_count = PAGE_SIZE / slab_info[i].size;
+            slab_info[i].page[j].used_count = 0;
+            for(int k = 0; k < 128; k++)
+                slab_info[i].page[j].is_used[k] = false;
+        }
 #if DEBUG
         printf("Slab %d: start = %p, end = %p, size = %d\n", i, slab_info[i].start, slab_info[i].end, slab_info[i].size);
+        printf("obj_size = %d, obj_count = %d\n", slab_info[i].page[0].obj_size, slab_info[i].page[0].obj_count);
 #endif
-
     }
+
 
 }
 
