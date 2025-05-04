0bb50aee1fd2b8ea31324f0d2c3ae528885813e6
Dydong233 <1935569700@qq.com>
2025-05-04 21:30:51 +0800

    L1

--- kernel/build/x86_64-qemu/src/os.d
+++
@@ -1,9 +0,0 @@
-/home/dydong/pwn/os-workbench/kernel/build/x86_64-qemu/./src/os.o: \
- /home/dydong/pwn/os-workbench/kernel/src/os.c include/common.h \
- framework/kernel.h \
- /home/dydong/pwn/os-workbench/kernel/../abstract-machine/am/include/am.h \
- /home/dydong/pwn/os-workbench/kernel/../abstract-machine/am/include/arch/x86_64-qemu.h \
- /home/dydong/pwn/os-workbench/kernel/../abstract-machine/am/include/amdev.h \
- /home/dydong/pwn/os-workbench/kernel/../abstract-machine/klib/include/klib.h \
- /home/dydong/pwn/os-workbench/kernel/../abstract-machine/klib/include/klib-macros.h \
- include/test.h include/mutex.h
