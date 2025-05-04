0046b2d0b6848ab28cbd6a1914cc00a45101f4ed
tracer-nju <tracer@nju.edu.cn>
2025-05-04 14:26:37 +0800

    Linux dydong-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:26:37 up  3:46,  1 user,  load average: 1.49, 1.27, 0.82

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,8 +1,51 @@
 #include <common.h>
 
-static void *kalloc(size_t size) {
-    // TODO
-    // You can add more .c files to the repo.
+#define PAGE_SIZE 4096
+#define DEBUG 1
+
+static int isinit = 0;
+uintptr_t start_addr, end_addr, size;
+
+struct Slab_Info{
+    uintptr_t start;
+    uintptr_t end;
+    size_t size;
+
+}slab_info[5];
+
+int two_pow(int n)
+{
+    int ans = 1;
+    for(int i = 0; i < n; i++)
+        ans *= 2;
+    return ans;
+}
+void init_memory()
+{
+    start_addr = (uintptr_t)heap.start;
+    end_addr = (uintptr_t)heap.end;
+    size = end_addr - start_addr;
+    for(int i = 0; i < 5; i++)
+    {
+        slab_info[i].size = two_pow(i+5);
+        slab_info[i].start = start_addr + i * PAGE_SIZE * 1024;  // every slab is 4MB
+        slab_info[i].end = slab_info[i].start + PAGE_SIZE * 1024; // every slab is 4MB
+
+#if DEBUG
+        printf("Slab %d: start = %p, end = %p, size = %d\n", i, slab_info[i].start, slab_info[i].end, slab_info[i].size);
+#endif
+
+    }
+
+}
+
+
+static void *kalloc(size_t size)
+{
+    if(!isinit){
+        isinit = 1;
+        init_memory();
+    }
 
     return NULL;
 }
