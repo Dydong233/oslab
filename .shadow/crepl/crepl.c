36020305beeef2e042bd13f7addfcaf3227bae95
tracer-nju <tracer@nju.edu.cn>
2025-05-16 16:50:03 +0800

    Linux dydong-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:50:03 up  1:06,  1 user,  load average: 1.04, 0.73, 0.45

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -3,14 +3,21 @@
 
 int main(int argc, char *argv[]) {
     static char line[4096];
+    static char *type = "int";
 
     while (1) {
         printf("crepl> ");
         fflush(stdout);
-
         if (!fgets(line, sizeof(line), stdin))  break;
 
-        printf("%s", line);
+        if (memcmp(type,line,strlen(type)) == 0){
+            printf("Define a new function.\n");
+
+        }
+        else{
+            printf("Define a new variable.\n");
+            
+        }
 
         // To be implemented.
         printf("Got %zu chars.\n", strlen(line));
